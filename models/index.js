d07cc5a54f215d6187bfaff069d72408d3da0b95
moeinrahimi <imoeinrahimi1@gmail.com>
2018-12-26 17:42:25 +0330

    get needed data and insert to db

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Datastore = require('nedb')
 const db = {};
 
-db.prices = new Datastore({ filename: './prices', autoload: true });
+db.prices = new Datastore({ filename: './prices.db', autoload: true });
 db.prices.loadDatabase()
 module.exports.db=db
